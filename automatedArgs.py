8251c126cfb26a5b48a6c7cc79a5f7e803baf08f
Adelina <vozianuadelina@gmail.com>
2026-05-04 10:00:06 +0200

    restoring stashed changes

---
+++ automatedArgs.py
@@ -0,0 +1,8 @@
+
+from different import what
+import os
+
+def K_4Visualisation():
+    what("startingFile.json", 4, "C~", "C?", "C?", 1, "red", "y")
+def K_4():
+    what("startingFile.json", 4, "C~", "C?", "C?", 1, "red", "n")
--- test_filter.py
+++ test_filter.py
@@ -1,9 +1,11 @@
 import pytest
+#from startingConfigFile import user_friendly_solver
+from automatedArgs import K_4
 
-def testParsedArguments():
+def test_parsed_arguments(capsys):
+    K_4()
     # assert that parsed arguments are the given arguments
+    captured = capsys.readouterr()
+    #assert captured.out == "Do you already have a JSON file ready? [Y/n]\nThen a JSON file will be made with the next given arguments:\nWhat is the order of the starting graph?\nWhat is the starting graph in graph6 format?\n\nWhat is the graph6 format of the graph with which red starts?\nWhat is the graph6 format of the graph with which blue starts?\nHow many threads are allowed?\nWhich colour starts?\nWrite successful!\nYour JSON file is called 'startingFile.json'.\nDo you want to visualise the given graphs? [Y/n]\n\nYour graphs have been saved as .png files:\nStartingGraph.png\nRedGraph.png\nBlueGraph.png\n\n['The best graph is:\n', 'Color 1:\n', 'n010\n', '0n00\n', '10n0\n', '000n\n', 'Color 2:\n', 'n000\n', '0n00\n', '00n0\n', '000n\n', 'With fitness clique: 2\n', 'With fitness vertex capture: 2\n', 'With fitness max degree: 2\n']\n\nHistory of game saved.\n"
+    assert captured.out == ['The best graph is:\n', 'Color 1:\n', 'n010\n', '0n00\n', '10n0\n', '000n\n', 'Color 2:\n', 'n000\n', '0n00\n', '00n0\n', '000n\n', 'With fitness clique: 2\n', 'With fitness vertex capture: 2\n', 'With fitness max degree: 2\n']
     
-
-    
-    captured = pytest.capsys.readouterr()
-    assert captured.out == "Hello, Alice!\n"
