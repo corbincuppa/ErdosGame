944c1072f763fcbe598b4882b5d7da461ee8197c
csvBoglarka <csvboglarka@gmail.com>
2026-05-04 14:49:07 +0200

    added test_makeJSONFile() and the restructured startingConfigFile

--- startingConfigFile.py
+++ startingConfigFile.py
@@ -4,97 +4,58 @@ import time
 import networkx as nx
 import matplotlib.pyplot as plt
 
+def makeJSONFile(n, starting_graph, red_graph, blue_graph, thread_number, starting_player, bias):
+    # Make the JSON format
+    data = { "n" : n, 
+            "starting-graph" : starting_graph,
+            "red-graph" : red_graph, 
+            "blue-graph" : blue_graph, 
+            "threadnumber" : thread_number, 
+            "starting-player" : starting_player,
+            "bias" : bias
+    }
 
-def user_friendly_solver():
-    # Ask if user wants to make a JSON file or not
-    boolean_already_JSON = input("Do you already have a JSON file ready? [Y/n]\n")
-    while boolean_already_JSON.upper() != "Y" and boolean_already_JSON.upper() != "N":
-        print("\nThat is not an answer.\n")
-        boolean_already_JSON = input("Do you already have a JSON file ready? [Y/n]\n")
-            
-    if boolean_already_JSON.upper() == "N":
-        print("Then a JSON file will be made with the next given arguments:")
-        # Ask for the given arguments to be put in the JSON config file
-        n_In = int(input("What is the order of the starting graph?\n"))
-        starting_graph = input("What is the starting graph in graph6 format?\n")
-        red_graph_In = input("What is the graph6 format of the graph with which red starts?\n")
-        blue_graph_In = input("What is the graph6 format of the graph with which blue starts?\n")
-        thread_number_In = int(input("How many threads are allowed?\n"))
-        start_player = input("Which colour starts?\n")
+    # Dump the data onto the JSON file
+    myJSON = json.dumps(data)
+    with open("startingFile.json", "x") as jsonfile:
+        jsonfile.write(myJSON)
+        print("Write successful!\n")
+        print("Your JSON file is called 'startingFile.json'.\n")
 
-        # Change the variable value based on the colour chosen
-        starting_player_In = 0
-        if start_player.upper() == "BLUE":
-            starting_player_In = 2
-        elif start_player.upper() == "RED":
-            starting_player_In = 1
+def visualisationGraphs(file_name):
+     # Open that file 
+    with open(file_name, "r") as f:
+        data = json.load(f)
+        n = data["n"]
+       
 
-        # Make the JSON format
-        data = { "n" : n_In, 
-                "starting-graph" : starting_graph,
-                "red-graph" : red_graph_In, 
-                "blue-graph" : blue_graph_In, 
-                "threadnumber" : thread_number_In, 
-                "starting-player" : starting_player_In
-        }
+        # Make a new file which contains the given graphs in graph6 format
+        with open("fileWithGraphString", "x") as g:
+            g.write(data["starting-graph"]+"\n")
+            g.write(data["red-graph"]+"\n")
+            g.write(data["blue-graph"])
 
-        # Dump the data onto the JSON file
-        myJSON = json.dumps(data)
-        with open("startingFile.json", "x") as jsonfile:
-            jsonfile.write(myJSON)
-            print("Write successful!\n")
-            print("Your JSON file is called 'startingFile.json'.\n")
+    graph = nx.read_graph6(g.name)
 
-        # If the user doesn't already have one, make the file_name variable the created JSON file
-        file_name = "startingFile.json"
-
-    # If the user has a JSON file, then ask for its name
-    elif boolean_already_JSON.upper() == "Y":
-            # Ask for the name of the JSON file
-            file_name = input("What is the name of your JSON file?\n")
-
-    # Ask for optional visualisation
-    boolean_visualisation = input("Do you want to visualise the given graphs? [Y/n]\n")
-    while boolean_visualisation.upper() != "Y" and boolean_visualisation.upper() != "N":
-        print("\nThat is not an answer.\n")
-        boolean_visualisation = input("Do you want to visualise the given graphs? [Y/n]\n")
-
-    if boolean_visualisation.upper() == "Y":
-        # Open that file 
-        with open(file_name, "r") as f:
-            data = json.load(f)
-            n = data["n"]
-        
-
-            # Make a new file which contains the given graphs in graph6 format
-            with open("fileWithGraphString", "x") as g:
-                g.write(data["starting-graph"]+"\n")
-                g.write(data["red-graph"]+"\n")
-                g.write(data["blue-graph"])
-
-        graph = nx.read_graph6(g.name)
-
-        # Generate each graph
-        color = ["green", "red","blue"]
-        title = ["Starting", "Red", "Blue"]
-        clarifying_str = "\nYour graphs have been saved as .png files:\n"
-        for i in range(3):
-            fig, ax = plt.subplots()
-            nx.draw(graph[i], node_color=color[i], edge_color=color[i], ax=ax)
-            ax.set_title(f"{title[i]} graph")
-            # Save each graph as a file 
-            plt.savefig(f"{title[i]}Graph.png")
-            clarifying_str += f"{title[i]}Graph.png\n"
-        print(clarifying_str)    
+    # Generate each graph
+    color = ["green", "red","blue"]
+    title = ["Starting", "Red", "Blue"]
+    clarifying_str = "\nYour graphs have been saved as .png files:\n"
+    for i in range(3):
+        fig, ax = plt.subplots()
+        nx.draw(graph[i], node_color=color[i], edge_color=color[i], ax=ax)
+        ax.set_title(f"{title[i]} graph")
+        # Save each graph as a file 
+        plt.savefig(f"{title[i]}Graph.png")
+        clarifying_str += f"{title[i]}Graph.png\n"
+    print(clarifying_str)    
 
 
-        # Visualise each graph
-        for i in range(3):
-            os.system(f"open {title[i]}Graph.png")
+    # Visualise each graph
+    for i in range(3):
+        os.system(f"open {title[i]}Graph.png")
 
-    elif boolean_visualisation.upper() == "NO":
-        print("\nOK.\nHere are the results of the graph solver:")
-
+def writeHistory(file_name):
     # Open the given file and parse the arguments to the software
     with open(file_name, "r") as f:
         data = json.load(f)
@@ -105,9 +66,10 @@ def user_friendly_solver():
         blue_graph = data["blue-graph"]
         threads = data["threadnumber"]
         starting_player = data["starting-player"]
+        bias = data["bias"]
 
         # Call the solver function, save the terminal output path to a new file "pathToResult.txt"
-        os.system(f"bash Erdos-Game-Generic.sh {n} {starting_graph} {red_graph} {blue_graph} {threads} {starting_player} >> pathToResult.txt")
+        os.system(f"bash Erdos-Game-Generic.sh {n} {starting_graph} {red_graph} {blue_graph} {threads} {starting_player} {bias} >> pathToResult.txt")
 
         # make the current time of when the game was processed for history file making
         cur = time.ctime(time.time())
@@ -126,22 +88,80 @@ def user_friendly_solver():
         # date and time of when the game was processed
         # <timestamp>\t<base graph>\t<red graph>\t<blue graph>\t
 
-        # game bias, temporary
-        bias = 0
-
         # player that wins
-        win = result[-2].split(" ")
+        win = result[-3].split(" ")
         win = win[-1].split("\n")
         win = win[0]
         win = int(win)
         if win > 10:
             winning_player = "red"
+            score_red = win//11
+            score_blue = win%11
         else:
             winning_player = "blue"
+            # score
+            score_red = win
+            score_blue = win
+        score = f"{score_red} - {score_blue}"
 
-        # final score, example
-        score = "2 - 10"
+        # write the history of the game to the file
         j.write(f"{cur}\t{starting_graph}\t{red_graph}\t{blue_graph}\t{bias}\t{winning_player}\t{score}\n")
-        print("\nHistory of game saved.\n")
 
-user_friendly_solver()
+
+
+
+
+
+
+
+def user_friendly_solver():        
+    # Ask if user wants to make a JSON file or not
+    boolean_already_JSON = input("Do you already have a JSON file ready? [Y/n]\n")
+    while boolean_already_JSON.upper() != "Y" and boolean_already_JSON.upper() != "N":
+        print("\nThat is not an answer.\n")
+        boolean_already_JSON = input("Do you already have a JSON file ready? [Y/n]\n")
+            
+    if boolean_already_JSON.upper() == "N":
+        print("Then a JSON file will be made with the next given arguments:")
+        # Ask for the given arguments to be put in the JSON config file
+        n_In = int(input("What is the order of the starting graph?\n"))
+        starting_graph = input("What is the starting graph in graph6 format?\n")
+        red_graph_In = input("What is the graph6 format of the graph with which red starts?\n")
+        blue_graph_In = input("What is the graph6 format of the graph with which blue starts?\n")
+        thread_number_In = int(input("How many threads are allowed?\n"))
+        start_player_In = input("Which colour starts?\n")
+        # game bias, ask how many extra edges blue colours.
+        bias = int(input("For each edge that red colours, how many extra edges can blue colour? [0 if they colour equally]\n"))
+
+        # Change the variable value based on the colour chosen
+        start_player = 0
+        if start_player_In.upper() == "BLUE":
+            start_player = 2
+        elif start_player_In.upper() == "RED":
+            start_player = 1
+
+        makeJSONFile(n_In, starting_graph, red_graph_In, blue_graph_In, thread_number_In, start_player, bias)
+
+        # If the user doesn't already have one, make the file_name variable the created JSON file
+        file_name = "startingFile.json"
+
+    # If the user has a JSON file, then ask for its name
+    elif boolean_already_JSON.upper() == "Y":
+            # Ask for the name of the JSON file
+            file_name = input("What is the name of your JSON file?\n")
+
+    # Ask for optional visualisation
+    boolean_visualisation = input("Do you want to visualise the given graphs? [Y/n]\n")
+    while boolean_visualisation.upper() != "Y" and boolean_visualisation.upper() != "N":
+        print("\nThat is not an answer.\n")
+        boolean_visualisation = input("Do you want to visualise the given graphs? [Y/n]\n")
+
+    if boolean_visualisation.upper() == "Y":
+        visualisationGraphs(file_name)
+
+    elif boolean_visualisation.upper() == "NO":
+        print("\nOK.\nHere are the results of the graph solver:")
+
+    writeHistory(file_name)
+
+#user_friendly_solver()
--- test_filter.py
+++ test_filter.py
@@ -1,11 +1,29 @@
 import pytest
-#from startingConfigFile import user_friendly_solver
-from automatedArgs import K_4
+import json
+import os
+from startingConfigFile import makeJSONFile, visualisationGraphs
 
-def test_parsed_arguments(capsys):
-    K_4()
-    # assert that parsed arguments are the given arguments
-    captured = capsys.readouterr()
-    #assert captured.out == "Do you already have a JSON file ready? [Y/n]\nThen a JSON file will be made with the next given arguments:\nWhat is the order of the starting graph?\nWhat is the starting graph in graph6 format?\n\nWhat is the graph6 format of the graph with which red starts?\nWhat is the graph6 format of the graph with which blue starts?\nHow many threads are allowed?\nWhich colour starts?\nWrite successful!\nYour JSON file is called 'startingFile.json'.\nDo you want to visualise the given graphs? [Y/n]\n\nYour graphs have been saved as .png files:\nStartingGraph.png\nRedGraph.png\nBlueGraph.png\n\n['The best graph is:\n', 'Color 1:\n', 'n010\n', '0n00\n', '10n0\n', '000n\n', 'Color 2:\n', 'n000\n', '0n00\n', '00n0\n', '000n\n', 'With fitness clique: 2\n', 'With fitness vertex capture: 2\n', 'With fitness max degree: 2\n']\n\nHistory of game saved.\n"
-    assert captured.out == ['The best graph is:\n', 'Color 1:\n', 'n010\n', '0n00\n', '10n0\n', '000n\n', 'Color 2:\n', 'n000\n', '0n00\n', '00n0\n', '000n\n', 'With fitness clique: 2\n', 'With fitness vertex capture: 2\n', 'With fitness max degree: 2\n']
-    
+def test_makeJSONFile():
+    makeJSONFile(
+        n=4,
+        starting_graph="C~",
+        red_graph="C?",
+        blue_graph="C?",
+        thread_number=1,
+        starting_player=1,
+        bias=0
+    )
+
+    # Controleer of het bestand bestaat
+    assert os.path.exists("startingFile.json")
+
+    with open("startingFile.json", "r") as f:
+        data = json.load(f)
+
+    assert data["n"] == 4
+    assert data["starting-graph"] == "C~"
+    assert data["red-graph"] == "C?"
+    assert data["blue-graph"] == "C?"
+    assert data["threadnumber"] == 1
+    assert data["starting-player"] == 1
+    assert data["bias"] == 0
