08ac29fc2ef1148c38897d3c3784e483f4428157
Adelina <vozianuadelina@gmail.com>
2026-05-07 14:10:54 +0200

    added more comments and now shows list of backup files

--- restoreBackup.py
+++ restoreBackup.py
@@ -1,7 +1,10 @@
 import os
 
 def restoreBackup():
-    name_backup = input("What backup file would you like to restore the history.txt file to?\n")
+    # show all the backup files
+    os.system("ls ~/.played-games")
+    # ask which to restore history.txt back to
+    name_backup = input("\nWhat backup file would you like to restore the history.txt file to?\n")
 
     # full path to the backup file
     backup_file = os.path.expanduser(f"~/.played-games/{name_backup}")
@@ -9,6 +12,7 @@ def restoreBackup():
     # full path to history.txt
     history = os.path.expanduser("~/ErdosGame/history.txt")
 
+    # copy contents of backup file into history.txt file
     with open(backup_file, "r") as b:
         lines = b.readlines()
         with open(history, "w") as h:
