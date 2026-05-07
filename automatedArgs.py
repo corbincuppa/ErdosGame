3987aa9a5b61d16ea63e53d58b98bc41ef028fcc
Adelina <vozianuadelina@gmail.com>
2026-05-07 14:10:16 +0200

    deleted old temp files

--- automatedArgs.py
+++
@@ -1,8 +0,0 @@
-
-from different import what
-import os
-
-def K_4Visualisation():
-    what("startingFile.json", 4, "C~", "C?", "C?", 1, "red", "y")
-def K_4():
-    what("startingFile.json", 4, "C~", "C?", "C?", 1, "red", "n")
